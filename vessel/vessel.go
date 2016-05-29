a47bde42d5712d3b002d96beee21dbfdea8deeb4
Luke Murray <git@lukemurray.co.uk>
2016-05-29 17:08:46 +0100

    Added join function.

--- vessel/vessel.go
+++ vessel/vessel.go
@@ -1,11 +1,65 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"log"
 	"net"
+	"time"
 
 	"github.com/LSFN/lsfn/vessel/environment"
+	"github.com/LSFN/lsfn/vessel/protobuf"
 )
+
+const (
+	NET_RATE         = 60 // Updates per second sent to environment
+	NET_PERIOD       = (1000 / NET_RATE) * time.Millisecond
+	PROTOCOL_VERSION = "0.0.1"
+)
+
+func join(environmentConn *environment.Conn) (string, error) {
+	ticker := time.NewTicker(NET_PERIOD)
+	defer ticker.Stop()
+	stop := make(chan bool)
+	defer func() {
+		stop <- true
+	}()
+	joinMessage := &protobuf.VesselToEnvironment{
+		ProtocolVersion: PROTOCOL_VERSION,
+		JoinRequest:     &protobuf.JoinRequest{},
+	}
+
+	// A goroutine to repeatedly send a join request
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				environmentConn.Outbound <- joinMessage
+			case <-stop:
+				break
+			}
+		}
+	}()
+
+	for msg := range environmentConn.Inbound {
+		// Protocol version check
+		if msg.ProtocolVersion == "" {
+			return "", errors.New("Protocol version missing from packet received from environment")
+		} else if msg.ProtocolVersion != PROTOCOL_VERSION {
+			return "", errors.New("Protocol version mismatch. Ours: \"" + PROTOCOL_VERSION + "\", theirs: \"" + msg.ProtocolVersion + "\"")
+		}
+
+		joinResponse := msg.GetJoinResponse()
+		if joinResponse != nil {
+			if joinResponse.JoinSuccessful {
+				return joinResponse.VesselID, nil
+			} else {
+				break
+			}
+		}
+	}
+	return "", errors.New("Join unsuccessful")
+}
 
 func main() {
 	var environmentIPStr string
@@ -23,10 +77,17 @@ func main() {
 		Port: environmentPort,
 	}
 
-	_, err := environment.ConnectToEnvironment(environmentUDPAddress)
+	environmentConn, err := environment.ConnectToEnvironment(environmentUDPAddress)
 
 	if err != nil {
-		return
+		log.Fatalln(err)
 	}
 
+	id, err := join(environmentConn)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Joined with ID " + id)
 }
